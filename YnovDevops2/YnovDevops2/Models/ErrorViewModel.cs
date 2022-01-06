da7de6201a9615a6eefe0b8dd914439d429194cb
Alexandre Teixeira <askmethatfr@gmail.com>
2022-01-06 12:06:42 +0100

    init

---
+++ YnovDevops2/YnovDevops2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace YnovDevops2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
